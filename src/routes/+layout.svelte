98fd9bb071be4ed062a66d30fc4a8aa210567f23
bananabrann <plbrannan95@gmail.com>
2023-06-02 15:20:36 -0500

    Basic nav

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,47 @@
 <script>
   import "../app.scss";
+  import Github from "svelte-material-icons/Github.svelte";
+  import TelevisionClassic from "svelte-material-icons/TelevisionClassic.svelte";
+
   const version = "1.1.0";
 </script>
 
 <nav>
-  <a href="">View source code</a>
-  <a href="">Report an issue</a>
-  <a href={`https://github.com/bananabrann/tv.brannan.cloud/releases/tag/v&#x24;{version}`}>{version}</a
-  >
+  <div>
+    <!-- <a href="">View source code</a> -->
+    <a href="/"> Home</a>
+    <span>|</span>
+    <a href="/ask">Ask Lee</a>
+  </div>
+
+  <div>
+    <span class="github-nav-icon">
+      <a target="_blank" href="https://github.com/bananabrann/tv.brannan.cloud">
+        <Github size="1.75rem" />
+      </a>
+    </span>
+    <span>{version}</span>
+  </div>
+  <!-- <a href={`https://github.com/bananabrann/tv.brannan.cloud/releases/tag/v&#x24;{version}`}>{version}</a -->
 </nav>
+
 <slot />
+
+<style lang="scss">
+  nav {
+    display: flex;
+    justify-content: space-between;
+
+    > div {
+      display: flex;
+      align-items: center;
+      gap: 1vw;
+    }
+  }
+
+  .github-nav-icon {
+    a {
+      color: inherit;
+    }
+  }
+</style>
