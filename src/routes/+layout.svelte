7c0c9fdaf81f1c15907a5fde869a44a5c70d2996
bananabrann <plbrannan95@gmail.com>
2023-06-02 16:22:44 -0500

    Streaming services component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,28 +6,36 @@
   const version = "1.1.0";
 </script>
 
-<nav>
-  <div>
-    <!-- <a href="">View source code</a> -->
-    <a href="/"> Home</a>
-    <span>|</span>
-    <a href="/ask">Ask Lee</a>
-  </div>
+<div class="line"></div>
 
-  <div>
-    <span class="github-nav-icon">
-      <a target="_blank" href="https://github.com/bananabrann/tv.brannan.cloud">
-        <Github size="1.75rem" />
-      </a>
-    </span>
-    <span>{version}</span>
-  </div>
-  <!-- <a href={`https://github.com/bananabrann/tv.brannan.cloud/releases/tag/v&#x24;{version}`}>{version}</a -->
-</nav>
+<main>
+  <nav>
+    <div>
+      <!-- <a href="">View source code</a> -->
+      <a href="/"> Home</a>
+      <span>|</span>
+      <a href="/ask">Ask Lee</a>
+    </div>
+  
+    <div>
+      <span class="github-nav-icon">
+        <a target="_blank" href="https://github.com/bananabrann/tv.brannan.cloud">
+          <Github size="1.75rem" />
+        </a>
+      </span>
+      <span>{version}</span>
+    </div>
+    <!-- <a href={`https://github.com/bananabrann/tv.brannan.cloud/releases/tag/v&#x24;{version}`}>{version}</a -->
+  </nav>
+  
+  <slot />
+</main>
 
-<slot />
 
 <style lang="scss">
+  main {
+    margin: 0.25rem 1rem;
+  }
   nav {
     display: flex;
     justify-content: space-between;
@@ -43,5 +51,12 @@
     a {
       color: inherit;
     }
+  }
+
+  .line {
+    margin: 0;
+    padding: 0;
+    height: 0.25rem;
+    background-color: $brand-primary-color;
   }
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,55 @@
-<h1>Welcome to SvelteKit</h1>
+<script>
+  import { fly } from "svelte/transition";
+  import StreamingService from "../StreamingService.svelte";
+</script>
+
+<br />
+<br />
+
+
+<section id="streaming-providers-container">
+  <StreamingService
+    name="Paramount"
+    linkUrl="https://www.paramountplus.com/"
+    imageUrl="https://files.brannan.cloud/tv/paramount-450x174.jpg"
+  />
+
+  <StreamingService
+    name="Netflix"
+    linkUrl="https://www.netflix.com/"
+    imageUrl="https://files.brannan.cloud/tv/netflix-450x174.jpg"
+  />
+
+  <StreamingService
+    name="Hulu"
+    linkUrl="https://www.hulu.com/"
+    imageUrl="https://files.brannan.cloud/tv/hulu-450x174.jpg"
+  />
+
+  <StreamingService
+    name="YouTube"
+    linkUrl="https://tv.youtube.com/welcome/"
+    imageUrl="https://files.brannan.cloud/tv/youtube-450x174.jpg"
+  />
+</section>
+
+<section />
 
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style>
+  #streaming-providers-container {
+    /* max-width: 80%; */
+    margin: 0 auto;
+    display: flex;
+    align-items: flex-start;
+    justify-content: center;
+    flex-wrap: wrap;
+    flex-basis: 300px;
+    gap: 4vw;
+
+    @media (max-width: 800px) {
+      gap: 1rem;
+    }
+  }
+</style>
