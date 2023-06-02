408bb4fc5b3f29c35b8a1e2d3040c9d13431c487
bananabrann <plbrannan95@gmail.com>
2023-06-02 14:40:14 -0500

    Base styling and Sveltekit init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+  import "../app.scss";
+  const version = "1.1.0";
+</script>
+
+<nav>
+  <a href="">View source code</a>
+  <a href="">Report an issue</a>
+  <a href={`https://github.com/bananabrann/tv.brannan.cloud/releases/tag/v&#x24;{version}`}>{version}</a
+  >
+</nav>
+<slot />
---
+++ src/routes/styles-testing/+page.svelte
@@ -0,0 +1,33 @@
+<h1>Header 1</h1>
+<h2>Header 2</h2>
+<h3>Header 3</h3>
+<h4>Header 4</h4>
+<h5>Header 5</h5>
+<h6>Header 6</h6>
+
+<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea quisquam magni nemo voluptatem fugit quam deserunt earum velit numquam animi, veritatis autem delectus vero fuga enim soluta odit ipsa. Corrupti.</p>
+
+<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam sed dignissimos atque, minima veritatis dolorum voluptate consequatur explicabo dicta officiis? Aliquid officia, fugit illum vel commodi laboriosam omnis aut repellendus?</p>
+
+<small>Small text here.</small>
+
+<p>Underlined text in p tags <u>here</u>.</p>
+<p>Bold text in p tags <b>here</b>.</p>
+<p>Italic text in p tags <i>here</i>.</p>
+<p>Span text in p tags <span>here</span>.</p>
+
+<br />
+
+<ul>
+  <li>List item 1</li>
+  <li>List item 2</li>
+  <li>List item 3</li>
+</ul>
+
+<a href="#">A link here.</a>
+
+<button>Button</button>
+
+<hr />
+
+<!-- TODO: Form elements -->
