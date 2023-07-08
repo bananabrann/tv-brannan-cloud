1074ab5ebb28d2f85f5cea5cd823f4682f4121f5
bananabrann <plbrannan95@gmail.com>
2023-07-08 11:13:32 -0500

    Add what's new page with previous updates

---
+++ src/routes/whats-new/+page.svelte
@@ -0,0 +1,69 @@
+<br>
+
+<h3>Version 1.1.11</h3>
+<ul>
+  <li>Added some secret things that help me know what's wrong if something breaks.</li>
+</ul>
+
+<div class="separator">
+  <small>Previous updates</small>
+  <hr />
+</div>
+
+<article>
+  <h3>Version 1.1.10</h3>
+  <code>July 3rd, 2023</code>
+
+  <ul>
+    <li>Fixed YouTube's link.</li>
+  </ul>
+</article>
+
+<article>
+  <h3>Versions 1.1.2 - 1.1.9</h3>
+  <ul>
+    <li>Multiple improvements that make updating your TV easier.</li>
+    <li>Your TV now costs less to run (under $3 a month).</li>
+  </ul>
+</article>
+
+<article>
+  <h3>Version 1.1.1</h3>
+  <code>June 10th, 2023</code>
+  <ul>
+    <li>Ask Lee now detects if you are grandma.</li>
+  </ul>
+</article>
+
+<article>
+  <h3>Version 1.1.0</h3>
+  <code>June 6th, 2023</code>
+  <ul>
+    <li>Backend improvements that make your TV more reliable and faster.</li>
+    <li>
+      A new <a href="/ask">Ask Lee</a> feature, allowing you to ask it questions on who has your favorite
+      TV shows and movies.
+    </li>
+    <li>New logo pictures.</li>
+  </ul>
+</article>
+
+<style lang="scss">
+  article {
+    h3 {
+      display: inline;
+    }
+  }
+
+  .separator {
+    margin: 3rem 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 1rem;
+
+    hr {
+      flex-grow: 1;
+    }
+  }
+</style>
