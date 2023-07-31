f539370b2c63aefe875e19dd951afa47ec0e07f7
bananabrann <plbrannan95@gmail.com>
2023-07-31 18:28:05 -0500

    Working storage with new way of doing it

--- src/lib/server/chat.ts
+++ src/lib/server/chat.ts
@@ -1,29 +1,24 @@
 import { version } from "$app/environment";
 import {
-  AZURE_STORAGE_ACCOUNT_NAME,
-  AZURE_STORAGE_SAS_TOKEN,
+  AZURE_STORAGE_CONNECTION_STRING,
   OPENAI_API_KEY,
   OPENAI_ORGANIZATION_ID
 } from "$env/static/private";
 import type ChatMessage from "$lib/types/ChatMessage.interface";
-import { getUniqueId } from "$lib/utils";
+import { createBlobFromString, downloadBlobToString, getUniqueId } from "$lib/utils";
 import type { AxiosError } from "axios";
 import moment from "moment";
 import { Configuration, OpenAIApi } from "openai";
-import { Readable } from "stream";
 
 import { BlobServiceClient } from "@azure/storage-blob";
 import { error } from "@sveltejs/kit";
 
 import contextPrompt from "./contextPrompt.json";
 
-const containerName = "tv-queries";
-const blobName = "logs.txt";
-
 console.log("Initializing Azure Blob Storage...");
 
-const blobServiceClient = new BlobServiceClient(
-  `https://${AZURE_STORAGE_ACCOUNT_NAME}.blob.core.windows.net?${AZURE_STORAGE_SAS_TOKEN}`
+const storageClient: BlobServiceClient = BlobServiceClient.fromConnectionString(
+  AZURE_STORAGE_CONNECTION_STRING
 );
 
 export const agentSessionId = getUniqueId();
@@ -38,19 +33,6 @@ const openai = new OpenAIApi(configuration);
 
 // Send a message to the OpenAI API. Returns the response message.
 export async function sendMessage(message: string, history: ChatMessage[] = [], ip: string = "") {
-  // Check if user is from a whitelisted IP
-  /*
-  if (WHITELISTED_USERS.split(" ").filter((x) => x === ip).length < 1) {
-    console.log(`User IP "${ip}" attempted to send a message, but is not whitelisted.`);
-
-    return {
-      role: "system",
-      content: "Error 403: User is not grandma"
-    } as ChatMessage;
-  }
-  */
-
-  // creating a log entry is definitely causing the 500 error
   createLogEntry(message, ip);
 
   try {
@@ -75,54 +57,14 @@ export async function sendMessage(message: string, history: ChatMessage[] = [], 
   }
 }
 
-function createLogEntry(message: string, ip: string = "none"): void {
-  console.log("createLogEntry");
-
-  let date = moment.utc();
-  let timestamp = date.utcOffset("-05:00").format("YYYY/MM/DD HH:mm [GMT]Z");
-
+async function createLogEntry(message: string, ip: string = "none"): Promise<void> {
+  const date = moment.utc();
+  const timestamp = date.utcOffset("-05:00").format("YYYY/MM/DD HH:mm [GMT]Z");
   const entry = `${timestamp} <${ip}@${agentSessionId}/${version}> ${message}`;
+  const containerClient = storageClient.getContainerClient("tv-files");
+  const blobName = "tv-chat.logs.txt";
+  const existingLogs: string = await downloadBlobToString(containerClient, blobName);
+  const newContent: string = existingLogs + "\n" + entry;
 
-  try {
-    appendLineAndUpdate(entry);
-  } catch (err: unknown) {
-    console.error(err);
-  }
-}
-
-async function appendLineAndUpdate(message: string) {
-  console.log("appendLineAndUpdate start");
-
-  const containerClient = blobServiceClient.getContainerClient(containerName);
-  const blobClient = containerClient.getBlockBlobClient(blobName);
-
-  console.log("Attempting to download blob...");
-  const downloadResponse = await blobClient.download(0);
-
-  if (!downloadResponse.readableStreamBody) {
-    console.error("Failed to download blob or blob was empty");
-    return;
-  }
-
-  let fileContent = (await streamToString(downloadResponse.readableStreamBody)) + "\n" + "New line" // message;
-
-  console.log("Attempting to upload blob...");
-  const uploadStream = Readable.from([fileContent]);
-  await blobClient.uploadStream(uploadStream, fileContent.length);
-  console.log("Blob uploaded successfully");
-}
-
-async function streamToString(readableStream: NodeJS.ReadableStream): Promise<string> {
-  console.log("streamToString");
-
-  return new Promise((resolve, reject) => {
-    const chunks: Array<any> = [];
-    readableStream.on("data", (data: any) => {
-      chunks.push(data.toString());
-    });
-    readableStream.on("end", () => {
-      resolve(chunks.join(""));
-    });
-    readableStream.on("error", reject);
-  });
+  createBlobFromString(containerClient, blobName, newContent);
 }
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,51 @@
+import type { BlobDownloadResponseParsed, ContainerClient } from "@azure/storage-blob";
+
 export function getUniqueId() {
   return Math.random().toString(36).substring(7);
+}
+
+export async function createBlobFromString(
+  client: ContainerClient,
+  blobName: string,
+  fileContentsAsString: string
+) {
+  const blockBlobClient = await client.getBlockBlobClient(blobName);
+
+  await blockBlobClient.upload(fileContentsAsString, fileContentsAsString.length);
+  console.log(`created blob ${blobName}`);
+}
+
+export async function downloadBlobToString(
+  containerClient: ContainerClient,
+  blobName: string
+): Promise<string> {
+  const blobClient = containerClient.getBlobClient(blobName);
+
+  const downloadResponse: BlobDownloadResponseParsed = await blobClient.download();
+
+  // TODO - Add type handling.
+  const downloaded: Promise<unknown> = (await streamToBuffer(
+    downloadResponse.readableStreamBody as unknown as ReadableStream
+  )) as Promise<unknown>;
+
+  return downloaded.toString();
+}
+
+// TODO - Add type handling.
+export async function streamToBuffer(readableStream: ReadableStream): Promise<unknown> {
+  return new Promise((resolve, reject) => {
+    //@ts-ignore
+    const chunks = [];
+    //@ts-ignore
+    readableStream.on("data", (data) => {
+      chunks.push(data instanceof Buffer ? data : Buffer.from(data));
+    });
+    //@ts-ignore
+    readableStream.on("end", () => {
+      //@ts-ignore
+      resolve(Buffer.concat(chunks));
+    });
+    //@ts-ignore
+    readableStream.on("error", reject);
+  });
 }
