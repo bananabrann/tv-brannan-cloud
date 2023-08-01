95515ac15b174bc4b4a881fbda4acb467d80b539
bananabrann <plbrannan95@gmail.com>
2023-07-31 21:13:37 -0500

    WIP - change utils to javascript, try removing typescript #48

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,36 +1,23 @@
 //@ts-nocheck
 
-import type { BlobDownloadResponseParsed, ContainerClient } from "@azure/storage-blob";
-
 export function getUniqueId() {
   return Math.random().toString(36).substring(7);
 }
 
-export async function createBlobFromString(
-  client: ContainerClient,
-  blobName: string,
-  fileContentsAsString: string
-) {
+export async function createBlobFromString(client, blobName, fileContentsAsString) {
   const blockBlobClient = await client.getBlockBlobClient(blobName);
 
   await blockBlobClient.upload(fileContentsAsString, fileContentsAsString.length);
   console.log(`created blob ${blobName}`);
 }
 
-export async function downloadBlobToString(
-  containerClient: ContainerClient,
-  blobName: string
-): Promise<string> {
+export async function downloadBlobToString(containerClient, blobName) {
   const blobClient = containerClient.getBlobClient(blobName);
 
-  const downloadResponse: BlobDownloadResponseParsed = await blobClient.download();
+  const downloadResponse = await blobClient.download();
 
-  // TODO - Add type handling.
-  const downloaded = await streamToBuffer(
-    downloadResponse.readableStreamBody
-  );
+  const downloaded = await streamToBuffer(downloadResponse.readableStreamBody);
 
-  // ts-ignore
   return downloaded.toString();
 }
 
