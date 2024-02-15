dd4d7e57ece0cec3973023ab7e41db4ee0246652
bananabrann <plbrannan95@gmail.com>
2024-02-15 13:11:05 -0600

    Add new test

--- src/index.test.ts
+++ src/index.test.ts
@@ -2,7 +2,12 @@ import { describe, it, expect } from "vitest";
 
 describe("sum test", () => {
   it("adds 1 + 2 to equal 3", () => {
-    expect(1 + 1).toBe(3);
+    expect(1 + 2).toBe(3);
   });
 });
 
+describe("hello test", () => {
+  it("says hello world", () => {
+    expect("hello world").toBe("hello world");
+  });
+});
