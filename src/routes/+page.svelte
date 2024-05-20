71917eea0f5828d1a6ab9a6576663d91b5e69b1e
bananabrann <plbrannan95@gmail.com>
2024-05-20 09:22:55 -0500

    Remove unused code and add it to an examples file

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,53 +1,19 @@
 <script>
-  // import ShowOfTheWeek from "$lib/components/ShowOfTheWeek.svelte";
   import StreamingService from "$lib/components/StreamingService.svelte";
 </script>
 
-<!-- prettier-ignore -->
-<!-- 
-  <ShowOfTheWeek
-    providerName="Hulu"
-    showName="Joy Ride"
-    recommendedBy="Lee and Alex"
-  />
- -->
-
 <section id="streaming-providers-container">
-  <!-- <StreamingService
-    name="Paramount"
-    linkUrl="https://www.paramountplus.com/"
-    imageUrl="https://files.brannan.cloud/tv-files/paramount-450x174.jpg"
-  /> -->
-
   <StreamingService
     name=""
     linkUrl="https://www.netflix.com/"
     imageUrl="https://files.brannan.cloud/tv-files/netflix-450x174.jpg"
   />
 
-  <!-- <StreamingService
-    name="Hulu"
-    linkUrl="https://www.hulu.com/"
-    imageUrl="https://files.brannan.cloud/tv-files/hulu-450x174.jpg"
-  /> -->
-
   <StreamingService
     name=""
     linkUrl="https://youtube.com/"
     imageUrl="https://files.brannan.cloud/tv-files/youtube-450x174.jpg"
   />
-
-  <!-- <StreamingService
-    name="ESPN on Hulu"
-    linkUrl="https://www.hulu.com/hub/sports"
-    imageUrl="https://files.brannan.cloud/tv-files/espn-450x174.jpg"
-  /> -->
-
-  <!-- <StreamingService
-    name="ABC News Live on Hulu"
-    linkUrl="https://www.hulu.com/hub/news-on-hulu"
-    imageUrl="https://files.brannan.cloud/tv-files/abcnewslive-450x174jpg.jpg"
-  /> -->
 </section>
 
 <style>
