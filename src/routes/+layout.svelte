c21da92de4f21bb7a598b16effc5882256790c42
bananabrann <plbrannan95@gmail.com>
2024-05-20 20:37:11 -0500

    Prettier enforcement

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,7 +11,7 @@
     <div>
       <a href="/"> Home</a>
       <span>|</span>
-      <a href="/whats-new">What's New</a>      
+      <a href="/whats-new">What's New</a>
       <span>|</span>
       <a href="/ask">Find A Show</a>
     </div>
